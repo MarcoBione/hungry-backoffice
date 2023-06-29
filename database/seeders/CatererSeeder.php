3265ffd20d9c9439e13397072fb7cd4962a8ab10
ChiaraS97 <chiara.sucato97@gmail.com>
2023-06-29 17:07:04 +0200

    edit CatererSeeder

--- database/seeders/CatererSeeder.php
+++ database/seeders/CatererSeeder.php
@@ -17,38 +17,7 @@ class CatererSeeder extends Seeder
     public function run()
     {
 
-        $caterers = [
-            [
-                'name' => 'Pizzeria Roma',
-                'address' => 'Via Roma, 1',
-                'image' => 'https://i.pinimg.com/564x/ae/5e/24/ae5e2403c0396bb3c16539f3879be22d.jpg',
-                'phone_number' => '02 7056 9901'
-            ],
-            [
-                'name' => 'Sushi Lab',
-                'address' => 'Via Milano, 2',
-                'image' => 'https://i.pinimg.com/564x/cc/8c/82/cc8c82057916ac5cb8ee7122e021e266.jpg',
-                'phone_number' => '02 2193 0011'
-            ],
-            [
-                'name' => 'Hong Kong',
-                'address' => 'Via Venezia, 3',
-                'image' => 'https://i.pinimg.com/564x/6d/be/82/6dbe82e9682e415915a5d9e7fb0e1c7b.jpg',
-                'phone_number' => '02 9821 6634'
-            ],
-            [
-                'name' => 'La Brace Food Experience',
-                'address' => 'Via Firenze, 4',
-                'image' => 'https://i.pinimg.com/564x/63/5e/3d/635e3d0fbec3931fba332535a6671a6a.jpg',
-                'phone_number' => '02 0034 5671 '
-            ],
-            [
-                'name' => 'Chiquito',
-                'address' => 'Via Napoli, 5',
-                'image' => 'https://i.pinimg.com/564x/86/bd/3c/86bd3c1bf93620167ab1485be959d3f2.jpg',
-                'phone_number' => '02 4768 1121'
-            ]
-        ];
+        $caterers = config('dataseeder.caterers');
 
         foreach ($caterers as $caterer) {
             $newCaterer = new Caterer();
