8acb4a931ce2a3990fc4a3d3e35ea7259fd70f4f
totarochristian <totaro.christian.18@gmail.com>
2023-06-29 16:25:52 +0200

    Add dataseeder file and paste the array defined in the order seeder

---
+++ config/dataseeder.php
@@ -0,0 +1,45 @@
+<?php
+return [
+    "orders" => [
+        [
+            "receiver" => "Francesco",
+            "phone_number" => "5163497403",
+            "status" => "Confermato",
+            "notes" => "Il campanello è rotto, urlare quando si è dal cancello",
+            "total_price" => 20.5,
+            "address" => "Giuseppe Sacconi, 5",
+        ],
+        [
+            "receiver" => "Luca",
+            "phone_number" => "3448496195",
+            "status" => "Confermato",
+            "notes" => "Bussare 3 volte di fila per favore",
+            "total_price" => 18.2,
+            "address" => "Via Veronese Guarino, 10",
+        ],
+        [
+            "receiver" => "Marta",
+            "phone_number" => "8987089354",
+            "status" => "Confermato",
+            "notes" => "Pulitevi le scarpe prima di entrare",
+            "total_price" => 15,
+            "address" => "Via Tito Livio, 3",
+        ],
+        [
+            "receiver" => "Erika",
+            "phone_number" => "1881621025",
+            "status" => "Confermato",
+            "notes" => "Non mandate Filiberto a fare la consegna, è un maleducato",
+            "total_price" => 6.9,
+            "address" => "Via Cadore, 10",
+        ],
+        [
+            "receiver" => "Luana",
+            "phone_number" => "7387907535",
+            "status" => "Confermato",
+            "notes" => "Ho fame, muovetevi",
+            "total_price" => 17.5,
+            "address" => "Via K.Adenauer, 3",
+        ]
+    ]
+];
