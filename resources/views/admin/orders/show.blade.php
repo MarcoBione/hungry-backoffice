5e88f37727d4f3e141ce8b8fc2843c855b99cf3c
totarochristian <totaro.christian.18@gmail.com>
2023-06-30 17:22:42 +0200

    Add containers with the preview of the caterer and the dishes ordered by the user

--- resources/views/admin/orders/show.blade.php
+++ resources/views/admin/orders/show.blade.php
@@ -28,23 +28,42 @@
             </div>
         </div>
         {{-- Container of the info about the order and the user that maded the order --}}
-        <div class="d-flex p-4 justify-content-center align-items-start">
+        <div class="d-flex p-4 justify-content-center align-items-start gap-4 flex-wrap">
             {{-- Container on the left side of the screen with the dishes info --}}
             <div class="d-flex flex-column align-items-center">
                 {{-- Container with the caterer data, the summary about the dishes and the total price --}}
-                {{-- <div class="d-flex gap-4 align-items-start border rounded-4 p-4">
-                    <i class="fa-solid fa-location-dot fs-3"></i>
-                    <div class="d-flex flex-column align-items-start justify-content-start">
-                        <span class="fs-5 fw-bold mb-2">Indirizzo di consegna</span>
-                        <div class="d-flex flex-column align-items-start">
-                            <span>{{ $order->receiver }}</span>
-                            <span>{{ $order->address }}</span>
+                <div class="d-flex flex-column gap-3 justify-content-start border rounded-4 p-4">
+                    {{-- Container with the data about the restaurant --}}
+                    <div class="d-flex gap-4">
+                        <img src="{{ $order->dishes[0]->caterer->image }}" alt="{{ $order->dishes[0]->caterer->name }}" class="img-thumbnail col-3">
+                        <div class="d-flex flex-column align-items-start justify-content-start">
+                            <span class="fs-5 fw-bold mb-2">{{ $order->dishes[0]->caterer->name }}</span>
+                            <div class="d-flex flex-column align-items-start">
+                                <span>{{ $order->dishes[0]->caterer->address }}</span>
+                                <span>{{ $order->dishes[0]->caterer->phone_number }}</span>
+                            </div>
                         </div>
                     </div>
-                </div> --}}
+                    <hr>
+                    {{-- Container with the dishes included in the order --}}
+                    <div class="d-flex flex-column gap-2">
+                        <span class="fs-5 fw-bold">Riepilogo dell'ordine</span>
+                        <div class="d-flex gap-2 flex-column align-items-start">
+                            @foreach ($order->dishes as $dish)
+                            <div class="d-flex justify-content-between align-items-center gap-4">
+                                <span>{{ $dish->name }} x {{ $dish->pivot->quantity }}</span>
+                                <span>{{ $dish->price * $dish->pivot->quantity }}&euro;</span>
+                                @if ($dish->pivot->notes)
+                                    <span>Note: {{ $dish->pivot->notes }}</span>
+                                @endif
+                            </div>
+                            @endforeach
+                        </div>
+                    </div>
+                </div>
             </div>
             {{-- Container on the right side of the screen with the info about the user --}}
-            <div class="d-flex flex-column align-items-center">
+            <div class="d-flex flex-column gap-4">
                 {{-- Container with the address --}}
                 <div class="d-flex gap-4 align-items-start border rounded-4 p-4">
                     <i class="fa-solid fa-location-dot fs-3"></i>
@@ -84,10 +103,7 @@
         </div>
     </div>
     {{--
-        <th scope="col">Destinatario</th>
         <th scope="col">Numero di telefono</th>
-        <th scope="col">Note</th>
         <th scope="col">Prezzo totale</th>
-        <th scope="col">Indirizzo</th>
     --}}
 @endsection
