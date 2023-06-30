475237cfe0ab86ef9df0e7804d6959a069d92d87
MarcoBione <marcobione.work@gmail.com>
2023-06-30 17:47:29 +0200

    try to add is_admin

--- database/migrations/2023_06_30_144929_add_is_admin_to_users_table.php
+++ database/migrations/2023_06_30_144929_add_is_admin_to_users_table.php
@@ -14,7 +14,7 @@ return new class extends Migration
     public function up()
     {
         Schema::table('users', function (Blueprint $table) {
-            $table->boolean('is_admin')->after('id');
+            $table->boolean('is_admin')->after('id')->default(false);
         });
     }
 
