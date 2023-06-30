fd8796491eb845b0756b665c68825566fb540107
totarochristian <totaro.christian.18@gmail.com>
2023-06-30 12:03:48 +0200

    Define the index method in the order controller (with pagination of 10 elements)

--- app/Http/Controllers/Admin/OrderController.php
+++ app/Http/Controllers/Admin/OrderController.php
@@ -16,7 +16,8 @@ class OrderController extends Controller
      */
     public function index()
     {
-        //
+        $orders = Order::paginate(10);
+        return view('admin.orders.index', compact('orders'));
     }
 
     /**
