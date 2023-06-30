1bf1a334d825899d75b2de8bd30ff329814b0ce8
totarochristian <totaro.christian.18@gmail.com>
2023-06-30 11:24:22 +0200

    Add dishes relationship in the order model

--- app/Models/Order.php
+++ app/Models/Order.php
@@ -10,4 +10,9 @@ class Order extends Model
     use HasFactory;
 
     protected $guarded = [];
+
+    public function dishes()
+    {
+        return $this->belongsToMany(Dish::class)->withPivot("quantity","notes");
+    }
 }
