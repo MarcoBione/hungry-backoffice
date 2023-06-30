4ae0727a434c2ca249e535785f54c5a05d25af05
totarochristian <totaro.christian.18@gmail.com>
2023-06-30 12:40:58 +0200

    Remove the return in the comments of the index method in the orders controller

--- app/Http/Controllers/Admin/OrderController.php
+++ app/Http/Controllers/Admin/OrderController.php
@@ -11,8 +11,6 @@ class OrderController extends Controller
 {
     /**
      * Display a listing of the resource.
-     *
-     * @return \Illuminate\Http\Response
      */
     public function index()
     {
