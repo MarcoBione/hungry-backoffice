1669289a85402dfaffafc46e2158b186124ff27f
ChiaraS97 <chiara.sucato97@gmail.com>
2023-06-30 12:27:54 +0200

    Caterers index.blade done

--- resources/views/admin/caterers/index.blade.php
+++ resources/views/admin/caterers/index.blade.php
@@ -3,7 +3,7 @@
 @section('content')
      <div class="container">
          <div class="text-end">
-             <a class="btn btn-success" href="{{ route('admin.caterers.create') }}">Aggiungi un ristorante</a>
+             <a class="btn btn-success mt-3" href="{{ route('admin.caterers.create') }}">Aggiungi un ristorante</a>
          </div>
          <div class="container d-flex flex-wrap gap-3 justify-content-start mt-5">
 
@@ -11,7 +11,7 @@
                  <div class="card" style="width: 18rem;">
                      <div class="card-body">
                          <h5 class="card-title">{{ $caterer->name }}</h5>
-                         <img class="card-img-top" :src="" alt="">
+                         <img class="card-img-top" style="width: 250px; height: 230px; object-fit: cover" src="{{$caterer->image}}" alt="{{$caterer->name}}">
                      </div>
                      <div class="card-body d-flex flex-column align-items-center gap-2 ">
                          <a href="{{ route('admin.caterers.show', $caterer->slug) }}"
