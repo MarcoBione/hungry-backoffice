aa0ea3dc28ee4cb46cbcb9140fa7741355184596
marikadiblasio <marbla.albram@gmail.com>
2023-07-04 17:09:48 +0200

    add storage to edit view

--- resources/views/admin/caterers/edit.blade.php
+++ resources/views/admin/caterers/edit.blade.php
@@ -37,19 +37,26 @@
                         </div>
                     @enderror
             </div>
-            <div class="mb-3 was-validated">
-                <label for="image">Immagine <span class="fs-7 text-warning-emphasis">*</span></label>
-                <input type="text" class="form-control @error('image') is-invalid @enderror" name="image"
-                    id="image" value="{{ old('name', $caterer->image) }}" required>
-                    @error('image')
-                        <span class="invalid-feedback" role="alert">
-                            <strong>{{ $message }}</strong>
-                        </span>
-                    @else
-                        <div class="invalid-feedback" role="alert">
-                            Per favore, inserisci l'immagine di copertina del ristorante
-                        </div>
-                    @enderror
+            <div class="d-flex">
+                <div class="mb-3 was-validated">
+                    <label for="image">Immagine <span class="fs-7 text-warning-emphasis">*</span></label>
+                    <input type="image" class="form-control @error('image') is-invalid @enderror" name="image"
+                        id="image" value="{{ old('name', $caterer->image) }}" required>
+                        @error('image')
+                            <span class="invalid-feedback" role="alert">
+                                <strong>{{ $message }}</strong>
+                            </span>
+                        @else
+                            <div class="invalid-feedback" role="alert">
+                                Per favore, inserisci l'immagine di copertina del ristorante
+                            </div>
+                        @enderror
+                </div>
+                <div class="media ms-4">
+                    <img id="uploadPreview" class="rounded" width="150"
+                        src="{{ $caterer->image ? asset('storage/' . $caterer->image) : 'https://via.placeholder.com/300x200' }}"
+                        alt="{{ $caterer->name }}">
+                </div>
             </div>
             <div class="mb-3 was-validated">
                 <label for="phone_number">Numero di telefono <span class="fs-7 text-warning-emphasis">*</span></label>
