2def8b5aa225c4b82adff565f86d131e59fa969d
totarochristian <totaro.christian.18@gmail.com>
2023-07-04 10:34:00 +0200

    Set the id to the register submit button

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -107,7 +107,7 @@
 
                             <div class="mb-4 row mb-0">
                                 <div class="col-md-6 offset-md-4">
-                                    <button type="submit" class="btn btn-primary">
+                                    <button type="submit" class="btn btn-primary" id="ajaxSubmit">
                                         {{ __('Register') }}
                                     </button>
                                 </div>
