bf79a525eb630ea41b8d78e4f67140c955f90a86
totarochristian <totaro.christian.18@gmail.com>
2023-07-04 14:02:49 +0200

    Update dishes create view: add * where required and the related messages and classes

--- resources/views/admin/dishes/create.blade.php
+++ resources/views/admin/dishes/create.blade.php
@@ -1,12 +1,15 @@
 @extends('layouts.app');
 @section('content')
     <div class="container mt-3">
-        <h1>Aggiungi un piatto</h1>
         <form action="{{ route('admin.dishes.store') }}" method="POST">
             @csrf
+            <h2 class="d-flex flex-column align-items-between flex-wrap mb-4">
+                <span class="fs-3">Creazione di un nuovo piatto</span>
+                <span class="fs-7 text-warning-emphasis">I campi contrassegnati con * sono obbligatori</span>
+            </h2>
             @if(Auth::user()->is_admin)
-            <div class="mb-3">
-                <label for="caterer_id">Scegli il caterer</label>
+            <div class="mb-3 was-validated">
+                <label for="caterer_id">Scegli il ristorante nel quale associare il piatto <span class="fs-7 text-warning-emphasis">*</span></label>
                 <select class="form-select mb-3" name="caterer_id" id="caterer_id">
                     <option value="">Seleziona il caterer</option>
                     @foreach ($caterers as $caterer)
@@ -15,58 +18,87 @@
                 </select>
             </div>
             @endif
-            <div class="mb-3">
-                <label for="name">Nome Piatto</label>
+            <div class="mb-3 was-validated">
+                <label for="name">Nome Piatto <span class="fs-7 text-warning-emphasis">*</span></label>
                 <input type="text" class="form-control @error('name') is-invalid @enderror" name="name" id="name"
                     required maxlength="255" minlength="3">
                 @error('name')
-                    <div class="invalid-feedback">{{ $message }}</div>
+                    <span class="invalid-feedback" role="alert">
+                        <strong>{{ $message }}</strong>
+                    </span>
+                @else
+                    <div class="invalid-feedback" role="alert">
+                        Per favore, inserisci il nome del piatto
+                    </div>
                 @enderror
             </div>
-            <div class="mb-3">
+            <div class="mb-3 was-validated">
                 <label for="image">Immagine</label>
                 <input type="text" class="form-control @error('image') is-invalid @enderror" name="image"
                     id="image">
                 @error('image')
-                    <div class="invalid-feedback">{{ $message }}</div>
+                    <span class="invalid-feedback" role="alert">
+                        <strong>{{ $message }}</strong>
+                    </span>
+                @else
+                    <div class="invalid-feedback" role="alert">
+                        Per favore, inserisci l'immagine del piatto
+                    </div>
                 @enderror
             </div>
-            <div class="mb-3">
-                <label for="price">Prezzo</label>
+            <div class="mb-3 was-validated">
+                <label for="price">Prezzo <span class="fs-7 text-warning-emphasis">*</span></label>
                 <input type="number" class="form-control @error('price') is-invalid @enderror" name="price" required
                     id="price" min="0" step="any">
                 @error('price')
-                    <div class="invalid-feedback">{{ $message }}</div>
+                    <span class="invalid-feedback" role="alert">
+                        <strong>{{ $message }}</strong>
+                    </span>
+                @else
+                    <div class="invalid-feedback" role="alert">
+                        Per favore, inserisci il prezzo del piatto
+                    </div>
                 @enderror
             </div>
-            <div class="mb-3">
+            <div class="mb-3 was-validated">
                 <label for="description">Descrizione</label>
-                <textarea name="description" id="description" rows="10" class="form-control">
-                </textarea>
+                <textarea name="description" id="description" rows="10" class="form-control"></textarea>
             </div>
-            <div class="mb-3">
+            <div class="mb-3 was-validated">
                 <label for="tipologies">Tipologie</label>
                 <textarea name="tipologies" id="tipologies" rows="10"
-                    class="form-control @error('tipologies') is-invalid @enderror"></textarea>
+                    class="form-control @error('tipologies') is-invalid @enderror" required></textarea>
                 @error('tipologies')
-                    <div class="invalid-feedback">{{ $message }}</div>
+                    <span class="invalid-feedback" role="alert">
+                        <strong>{{ $message }}</strong>
+                    </span>
+                @else
+                    <div class="invalid-feedback" role="alert">
+                        Per favore, inserisci la tipologia del piatto
+                    </div>
                 @enderror
             </div>
-            <div class="form-check">
+            <div class="form-check was-validated">
                 <p>Visibile sul sito?</p>
                 <input class="form-check-input" type="radio" name="visible" id="true" value="true">
                 <label class="form-check-label" for="true">
                     Sì
                 </label>
             </div>
-            <div class="form-check">
+            <div class="form-check was-validated">
                 <input class="form-check-input" type="radio" name="visible" id="false" value="false" checked>
                 <label class="form-check-label" for="false">
                     No
                 </label>
             </div>
             @error('visible')
-                <div class="invalid-feedback">{{ $message }}</div>
+                <span class="invalid-feedback" role="alert">
+                    <strong>{{ $message }}</strong>
+                </span>
+            @else
+                <div class="invalid-feedback" role="alert">
+                    Per favore, inserisci la visibilità del piatto
+                </div>
             @enderror
             <button type="submit" class="btn btn-success">Save</button>
             <button type="reset" class="btn btn-primary">Reset</button>
