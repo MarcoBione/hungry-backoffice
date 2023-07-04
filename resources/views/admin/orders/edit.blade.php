b6a38ecbd30b9df0afa751fc51202f26dc6a64d9
totarochristian <totaro.christian.18@gmail.com>
2023-07-04 14:33:43 +0200

    Update orders edit view: add * where required and the related messages and classes

--- resources/views/admin/orders/edit.blade.php
+++ resources/views/admin/orders/edit.blade.php
@@ -2,13 +2,13 @@
 
 @section('content')
     <div class="container">
-        <h1 class="mt-3 mb-3">Modifica ordine {{ $order->id }} creato da {{ $order->receiver }} il {{ $order->created_at }}</h1>
         <form action="{{ route('admin.orders.update', $order->id) }}" method="POST" enctype="multipart/form-data" class="pb-4">
             @csrf
             @method('PUT')
-            <div class="mb-3">
-                <label for="status" class="form-label fs-5">Stato dell'ordine</label>
-                <select name="status" id="status" class="form-control @error('status') is-invalid @enderror">
+            <h2 class="mt-3 mb-3">Modifica <strong>ordine {{ $order->id }}</strong> creato da <strong>{{ $order->receiver }}</strong> il <strong>{{ $order->created_at }}</strong></h2>
+            <div class="mb-3 was-validated">
+                <label for="status" class="form-label fs-5">Stato dell'ordine <span class="fs-7 text-warning-emphasis">*</span></label>
+                <select name="status" id="status" class="form-control @error('status') is-invalid @enderror" required>
                     <option value="In attesa di conferma" {{ (old("status", $order->status) == "In attesa di conferma" ? "selected":"") }}>In attesa di conferma</option>
                     <option value="Ordine confermato" {{ (old("status", $order->status) == "Ordine confermato" ? "selected":"") }}>Ordine confermato</option>
                     <option value="Pagato" {{ (old("status", $order->status) == "Pagato" ? "selected":"") }}>Pagato</option>
@@ -18,24 +18,42 @@
                     <option value="Indirizzo di consegna errato" {{ (old("status", $order->status) == "Indirizzo di consegna errato" ? "selected":"") }}>Indirizzo di consegna errato</option>
                 </select>
                 @error('status')
-                    <div class="invalid-feedback">{{ $message }}</div>
+                    <span class="invalid-feedback" role="alert">
+                        <strong>{{ $message }}</strong>
+                    </span>
+                @else
+                    <div class="invalid-feedback" role="alert">
+                        Per favore, seleziona lo stato dell'ordine
+                    </div>
                 @enderror
             </div>
-            <div class="mb-3">
-                <label for="address" class="form-label fs-5">Indirizzo di consegna</label>
+            <div class="mb-3 was-validated">
+                <label for="address" class="form-label fs-5">Indirizzo di consegna <span class="fs-7 text-warning-emphasis">*</span></label>
                 <input type="text" class="form-control @error('address') is-invalid @enderror" name="address"
-                    id="address" value="{{ old('address', $order->address) }}">
+                    id="address" value="{{ old('address', $order->address) }}" required>
                 @error('address')
-                    <div class="invalid-feedback">{{ $message }}</div>
+                    <span class="invalid-feedback" role="alert">
+                        <strong>{{ $message }}</strong>
+                    </span>
+                @else
+                    <div class="invalid-feedback" role="alert">
+                        Per favore, indica l'indirizzo dove effettuare la consegna
+                    </div>
                 @enderror
             </div>
-            <div class="mb-3">
-                <label for="total_price" class="form-label fs-5">Prezzo totale (&euro;)</label>
+            <div class="mb-3 was-validated">
+                <label for="total_price" class="form-label fs-5">Prezzo totale (&euro;) <span class="fs-7 text-warning-emphasis">*</span></label>
                 <input type="number" step="0.01" value="{{ old('total_price', $order->total_price) }}"
                     class="form-control @error('total_price') is-invalid @enderror" id="total_price" name="total_price" min="0.1"
-                    max="9999.99">
+                    max="9999.99" required>
                 @error('total_price')
-                    <div class="invalid-feedback">{{ $message }}</div>
+                    <span class="invalid-feedback" role="alert">
+                        <strong>{{ $message }}</strong>
+                    </span>
+                @else
+                    <div class="invalid-feedback" role="alert">
+                        Per favore, indica il prezzo totale dell'ordine
+                    </div>
                 @enderror
             </div>
             <button type="submit" class="btn btn-success">Conferma</button>
