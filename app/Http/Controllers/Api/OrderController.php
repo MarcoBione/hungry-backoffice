048cd9ae24daa9ef08ae09b50487911057e6af36
marikadiblasio <marbla.albram@gmail.com>
2023-07-12 16:07:21 +0200

    changed receiver of NewOrder email

--- app/Http/Controllers/Api/OrderController.php
+++ app/Http/Controllers/Api/OrderController.php
@@ -15,7 +15,15 @@ class LeadController extends Controller
     public function store(Request $request){
          $data =  $request->validated();
          $order = Order::create($data);
-         Mail::to($order->receiver)->send(new NewOrder($order));
+
+         $userMail = DB::table("orders")->
+                join("dish_order","dish_order.order_id","=","orders.id")->
+                join("dishes","dishes.id","=","dish_order.dish_id")->
+                join("caterer", "caterers.id", "=", "dishes.caterer_id")->
+                join("users", "user.id", "=", "caterers.user_id")->
+                where("orders.id", $order->id)->get();
+
+         Mail::to($userMail)->send(new NewOrder($order));
          Mail::to($order->email)->send(new OrderComplete($order));
 
          return response()->json([
