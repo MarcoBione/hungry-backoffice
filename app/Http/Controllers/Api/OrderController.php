53c12fd14c42e8347a30ac97a17372728906e1e5
marikadiblasio <marbla.albram@gmail.com>
2023-07-12 11:58:10 +0200

    add order controller in api

---
+++ app/Http/Controllers/Api/OrderController.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+// use App\Models\Lead;
+use App\Models\Order;
+use Illuminate\Http\Request;
+use App\Mail\NewOrder;
+use App\Mail\OrderComplete;
+use Illuminate\Support\Facades\Mail;
+
+class LeadController extends Controller
+{
+    public function store(Request $request){
+         $data =  $request->validated();
+         $order = Order::create($data);
+
+         Mail::to('info@fashionshop.com')->send(new NewOrder($order));
+         Mail::to($order->email)->send(new OrderComplete($order));
+
+         return response()->json([
+            'success' => true,
+        ]);
+    }
+}
