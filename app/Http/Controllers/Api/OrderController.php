d3285fce2515c245f92f961f58f15faea3f2ce40
totarochristian <totaro.christian.18@gmail.com>
2023-07-12 16:47:40 +0200

    Define basic api order

--- app/Http/Controllers/Api/OrderController.php
+++ app/Http/Controllers/Api/OrderController.php
@@ -12,18 +12,25 @@ use Illuminate\Support\Facades\Mail;
 
 class LeadController extends Controller
 {
-    public function store(Request $request){
-         $data =  $request->validated();
-         $order = Order::create($data);
+    public function store($dataFromFront){
+        $order = new Order();
+        $order->receiver = $dataFromFront->receiver;
+        $order->phone_number = $dataFromFront->phoneNumber;
+        $order->notes = $dataFromFront->notes;
+        //mail
+        $order->save();
+        //Create relations con i dishes
 
-         $userMail = DB::table("orders")->
+         $catererMail = DB::table("orders")->
                 join("dish_order","dish_order.order_id","=","orders.id")->
                 join("dishes","dishes.id","=","dish_order.dish_id")->
                 join("caterer", "caterers.id", "=", "dishes.caterer_id")->
                 join("users", "user.id", "=", "caterers.user_id")->
                 where("orders.id", $order->id)->value('users.email');
 
-         Mail::to($userMail)->send(new NewOrder($order));
+        //send mail to the caterer
+         Mail::to($catererMail)->send(new NewOrder($order));
+        //send mail to the receiver
          Mail::to($order->email)->send(new OrderComplete($order));
 
          return response()->json([
