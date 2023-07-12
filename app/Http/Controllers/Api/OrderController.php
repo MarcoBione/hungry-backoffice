be55be69f3f5b8fc3aabe16a732552b653d3e5fc
marikadiblasio <marbla.albram@gmail.com>
2023-07-12 16:05:09 +0200

    fixed order receiver

--- app/Http/Controllers/Api/OrderController.php
+++ app/Http/Controllers/Api/OrderController.php
@@ -7,6 +7,7 @@ use App\Models\Order;
 use Illuminate\Http\Request;
 use App\Mail\NewOrder;
 use App\Mail\OrderComplete;
+use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Mail;
 
 class LeadController extends Controller
@@ -14,8 +15,7 @@ class LeadController extends Controller
     public function store(Request $request){
          $data =  $request->validated();
          $order = Order::create($data);
-
-         Mail::to('info@fashionshop.com')->send(new NewOrder($order));
+         Mail::to($order->receiver)->send(new NewOrder($order));
          Mail::to($order->email)->send(new OrderComplete($order));
 
          return response()->json([
