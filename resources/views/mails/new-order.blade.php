6df2edeec8c3411b6cae747886d05f90b3cfa350
marikadiblasio <marbla.albram@gmail.com>
2023-07-14 07:29:15 +0200

    add email

--- resources/views/mails/new-order.blade.php
+++ resources/views/mails/new-order.blade.php
@@ -1,6 +1,15 @@
-<h1>Complimenti {{$userName}}!!</h1>
+<h3>Complimenti {{$name}}!!</h3>
 <p>
     Hai ricevuto un nuovo ordine <br>
-    Accedi alla tua area riservata per i dettagli.<br>
-    Il team di Hungry
+    Indirizzo di consegna: {{$order->address}}.<br>
+    Riepilogo:
+    @foreach ($order->dishes as $dish)
+        <div>
+            {{$dish->pivot->quantity ? $dish->pivot->quantity : '1'}}
+            {{$dish->name}}
+        </div>
+    @endforeach
+    Totale pagato: {{$order->total_price}}<br>
 </p>
+<span>Accedi alla tua area riservata per ulteriori dettagli.</span>
+<h5>Il team di Hungry</h5>
--- resources/views/mails/order-complete.blade.php
+++ resources/views/mails/order-complete.blade.php
@@ -1,5 +1,15 @@
-<h1>Grazie per il tuo ordine</h1>
+<h3>{{$order->receiver}}, grazie per il tuo ordine</h3>
 <p>
-    Ordine effettuato con successo.
-    Sarà spedito all'indirizzo da te indicato a breve.
+    Il tuo ordine è effettuato con successo. <br>
+    Sarà spedito all'indirizzo: {{$order->address}}. <br>
+    Riepilogo:
+    @foreach ($order->dishes as $dish)
+        <div>
+            {{$dish->pivot->quantity ? $dish->pivot->quantity : '1'}}
+            {{$dish->name}}
+        </div>
+    @endforeach
+    Totale pagato: {{$order->total_price}} &euro;.
 </p>
+<span>Buon appetito!</span>
+<h5>Il team di Hungry</h5>
