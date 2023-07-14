0d37eeb2cc08bda27b47bc380327d952e922d432
marikadiblasio <marbla.albram@gmail.com>
2023-07-14 09:23:37 +0200

    add route for dashboard with index

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\Admin\DishController;
+use App\Http\Controllers\Admin\DashboardController;
 use App\Http\Controllers\Admin\OrderController;
 use App\Http\Controllers\Admin\CatererController;
 use App\Http\Controllers\Admin\CategoryController;
@@ -22,11 +23,12 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/admin', function () {
-    return view('dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
+// Route::get('/admin', function () {
+//     return view('dashboard');
+// })->middleware(['auth', 'verified'])->name('dashboard');
 
 Route::middleware(['auth', 'verified'])->name('admin.')->prefix('admin')->group(function () {
+    Route::get('/', [DashboardController::class, 'index'])->name('dashboard');
     Route::resource('dishes', DishController::class)->parameters(['dishes' => 'dish:slug']);
     Route::resource('orders', OrderController::class);
     Route::resource('caterers', CatererController::class)->parameters(['caterers' => 'caterer:slug']);
