bb51c167a6c0988110d425dbf97088cb531e02e4
MarcoBione <marcobione.work@gmail.com>
2023-07-14 15:43:57 +0200

    Set facivon + tab title

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -8,7 +8,7 @@
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
-    <title>{{ config('app.name', 'Hungry') }}</title>
+    <title>Hungry®️</title>
 
     <!-- Fonts -->
     <link rel="dns-prefetch" href="//fonts.gstatic.com">
