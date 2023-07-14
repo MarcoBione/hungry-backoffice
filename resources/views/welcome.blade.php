a01ca5bba02730511ef8a6a8e6f196f9daa2894c
marikadiblasio <marbla.albram@gmail.com>
2023-07-14 15:28:06 +0200

    fixed welcome page

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,9 +4,19 @@
     <div class="jumbotron bg-light rounded-3 d-flex flex-column align-items-center" style="height: 830px; padding-top: 100px">
         <img src="/images/Logo2.png" alt="">
         <div class="content">
+            @if(!Auth::check())
             <div class="container">
                 <p class="fs-3 text-center">Caro ristoratore, benvenuto nell'<b>Area Riservata</b> di <b>Hungry®️</b>! <br>
-                     Fai il <a href="/login">login</a> se sei già nostro cliente, oppure <a href="/register">registrati</a> per iniziare subito a gestire con semplicità la tua attività! </p>
+                     Fai il <a href="/login">login</a> se sei già nostro cliente, oppure <a href="/register">registrati</a> per iniziare subito a gestire con semplicità la tua attività!
+                </p>
             </div>
+            @endif
+            @if(Auth::check())
+            <div class="container">
+                <p class="fs-3 text-center">Caro ristoratore, benvenuto nell'<b>Area Riservata</b> di <b>Hungry®️</b>! <br>
+                    Vai alla <a href="/admin">Dashboard</a>!
+                </p>
+            </div>
+            @endif
         </div>
     @endsection
